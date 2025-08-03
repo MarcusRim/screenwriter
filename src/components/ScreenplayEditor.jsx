ce560042bc0f37decd0109eed2648e8fc0edd298
BuildTools <unconfigured@null.spigotmc.org>
2025-08-02 17:55:37 -0700

    use online pagination extension instead of custom

--- src/components/ScreenplayEditor.jsx
+++ src/components/ScreenplayEditor.jsx
@@ -1,9 +1,12 @@
 // src/components/ScreenplayEditor.jsx
 import React, { useState } from 'react'
 import { Box } from '@chakra-ui/react';
-import { useEditor, EditorContent, EditorProvider } from '@tiptap/react';
+import { useEditor, EditorContent } from '@tiptap/react';
 //import { Pagination } from 'tiptap-pagination-breaks';
-import { CustomPagination } from '../extensions/CustomPagination';
+//import { CustomPagination } from '../extensions/CustomPagination';
+import PaginationExtension, { 
+  PageNode, HeaderFooterNode, BodyNode }
+  from 'tiptap-extension-pagination';
 import StarterKit from '@tiptap/starter-kit';
 import './screenplay.css';
 
@@ -11,16 +14,20 @@ export default function ScreenplayEditor({
   value, onChange
 }) {
   const PAGE_HEIGHT = 1056 // 11 in
-  const [heightPx, setHeightPx] = useState(PAGE_HEIGHT)
 
   const editor = useEditor({
     extensions: [
       StarterKit,
-      CustomPagination.configure({
-        pageHeight: PAGE_HEIGHT, // 11 in
-        pageWidth: 816,   // 8.5 in
-        pageMargin: 96,   // 1 in
+      PaginationExtension.configure({
+        defaultPaperSize: 'letter',
+        pageAmendmentOptions: {
+          enableHeader: false,
+          enableFooter: false,
+        },
       }),
+      PageNode,
+      HeaderFooterNode,
+      BodyNode,
     ],
     content: value,
     editorProps: {
@@ -30,12 +37,6 @@ export default function ScreenplayEditor({
     },
     onUpdate: ({editor}) => {
       onChange(editor.getText());
-      const totalHeight = editor.view.dom.scrollHeight
-      const pages = Math.ceil(totalHeight / PAGE_HEIGHT)
-      const newHeight = pages * PAGE_HEIGHT
-      setHeightPx(newHeight)
-      editor.view.dom.style.height = '${newHeight}px'
-      console.log('heightPX = ', heightPx)
     },
   });
   
@@ -45,13 +46,18 @@ export default function ScreenplayEditor({
   }
 
   return (
-    <Box className="screenplay-container">
-      <EditorContent 
-        editor = {editor} 
-        style = {{
-          height: '${heightPx}px',
-          overflow: 'visible',
-        }}
+    <Box
+      className="screenplay-container"
+      bg="gray.100"
+      p="8"
+      h="100vh"
+      overflowY="auto"
+      mx="auto"
+      maxW="calc(8.5in + 4rem)"
+    >
+      <EditorContent
+        editor={editor}
+        className="screenplay-editor"
       />
     </Box>
   );
